af2e9e9fbdc1e1ab61aa1a821a7f03f69dac5cb9
hasbifadillah <hasbifadillah@outlook.com>
2022-09-27 12:06:14 +0700

    feat: Implement PWA

--- projects/client/next.config.js
+++ projects/client/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+});
+
+module.exports = withPWA(nextConfig);
